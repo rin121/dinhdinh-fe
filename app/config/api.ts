5040bfd97aa12dd1ce52786defa398782fea8361
KDuy <kduy121@gmail.com>
2025-06-23 18:37:46 +0700

    no message

--- app/config/api.ts
+++ app/config/api.ts
@@ -2,7 +2,7 @@
 export const API_CONFIG = {
   // BASE_URL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api',
   // BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://127.0.0.1:8000',
-  BASE_URL: process.env.NEXT_PUBLIC_API_URL || 'https://banhkemlagi/api',
+  BASE_URL: process.env.NEXT_PUBLIC_API_URL || 'https://banhkemlagi.com/api',
   BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://banhkemlagi.com',
   TIMEOUT: 10000, // 10 seconds
   RETRY_ATTEMPTS: 3,
